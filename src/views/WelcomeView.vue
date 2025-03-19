36c13b457e93c29c76fea0c9159274e185d52a38
altguerrero <josealberto.guerreroayala@gmail.com>
2025-03-19 11:46:30 -0500

    feat: Enhance WelcomeView with a new layout, welcome message, and navigation button

--- src/views/WelcomeView.vue
+++ src/views/WelcomeView.vue
@@ -1,3 +1,26 @@
 <template>
-  <h1>Welcome</h1>
+  <div class="container mx-auto h-screen flex flex-col justify-center items-center px-[1.875rem]">
+    <figure class="flex justify-center mb-[3.75rem]">
+      <img :src="image" alt="Welcome" />
+    </figure>
+
+    <div class="text-center w-full max-w-[35.625rem]">
+      <h1 class="text-[1.625rem] font-bold text-content-primary mb-[1.875rem]">
+        Welcome to Pokédex
+      </h1>
+      <p class="text-lg font-medium text-content-secondary">
+        The digital encyclopedia created by Professor Oak is an invaluable tool to Trainers in the
+        Pokémon world.
+      </p>
+
+      <router-link to="/favorites">
+        <Button class="mt-[3.125rem]">Get started</Button>
+      </router-link>
+    </div>
+  </div>
 </template>
+
+<script setup lang="ts">
+import image from '@/assets/images/welcome.png'
+import { Button } from '@/components/ui/button'
+</script>
